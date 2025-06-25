727604e8d87e335e13fb19994db71b8526ca5b5d
Lalit Chaudhari <lalitchaudhari003@gmail.com>
2025-06-25 15:55:46 +0530

    new change

--- src/Components/api.js
+++ src/Components/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5001/', // backend URL
-});
-
 // const api = axios.create({
-//   baseURL: 'https://quiz-app-backend-smoky.vercel.app/', // deployed backend URL
+//   baseURL: 'http://localhost:5001/', // backend URL
 // });
+
+const api = axios.create({
+  baseURL: 'https://quiz-app-backend-smoky.vercel.app/', // deployed backend URL
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
